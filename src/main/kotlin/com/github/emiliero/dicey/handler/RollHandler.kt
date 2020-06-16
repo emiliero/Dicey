17944901df49aa3e64ebf3d463019357a2f03e19
emiliero <emiliero@hiof.no>
2020-06-16 15:14:38 +0200

    Fixed roll-bug

--- src/main/kotlin/com/github/emiliero/dicey/handler/RollHandler.kt
+++ src/main/kotlin/com/github/emiliero/dicey/handler/RollHandler.kt
@@ -5,7 +5,7 @@ import java.security.SecureRandom
 fun randomNumberGenerator(cmdParameters: String): String {
     val range = handleRollCommandParameters(cmdParameters)
 
-    return if ((range.first() <= 0 || range.last() <= 0) || range.last() >= 2147483647 ) {
+    return if (range.first() < 0 || range.last() >= 2147483647 ) {
         "Please choose a maximum value, alternatively minimum and maximum value, between 1 and 2147483646"
     } else {
         val secureRandom = SecureRandom().nextInt((range[1] - range[0]) + 1) + range[0]
@@ -18,17 +18,21 @@ private fun handleRollCommandParameters(message: String): Array<Int> {
     var maxValue = 0
     var minValue = 0
 
+    println(messageCharArray.size)
+
     if (messageCharArray.size == 2) {
         maxValue = messageCharArray[1].toIntOrNull() ?: 0
+        println(maxValue)
+        println(minValue)
     } else if (messageCharArray.size >= 3) {
         minValue = messageCharArray[1].toIntOrNull() ?: 0
         maxValue = messageCharArray[2].toIntOrNull() ?: 0
+    }
 
-        if (minValue > maxValue) {
-            val temp = minValue
-            minValue = maxValue
-            maxValue = temp
-        }
+    if (minValue > maxValue) {
+        val temp = minValue
+        minValue = maxValue
+        maxValue = temp
     }
 
     return arrayOf(minValue, maxValue)
