a875ac7c13c3a6ee763d9cb225ceade713d37db9
emiliero <emiliero@hiof.no>
2020-07-05 21:16:56 +0200

    Bot has 'listening to' as Activity

--- src/main/kotlin/com/github/emiliero/dicey/Main.kt
+++ src/main/kotlin/com/github/emiliero/dicey/Main.kt
@@ -2,13 +2,19 @@ package com.github.emiliero.dicey
 
 import com.github.emiliero.dicey.authenticate.login
 import com.github.emiliero.dicey.controller.commands
+import com.github.emiliero.dicey.model.Commands
 import discord4j.core.DiscordClient
 import discord4j.core.DiscordClientBuilder
+import discord4j.core.`object`.presence.Activity
+import discord4j.core.`object`.presence.Presence
+
 
 fun main(args: Array<String>) {
-    val client: DiscordClient = DiscordClientBuilder(BuildConfig.TOKEN_KEY).build()
-    login(client)
+    val client: DiscordClient = DiscordClientBuilder(BuildConfig.TOKEN_KEY)
+        .setInitialPresence(Presence.online(Activity.listening(Commands.Cmds.command)))
+        .build()
 
+    login(client)
     commands(client)
     client.login().block()
 }
