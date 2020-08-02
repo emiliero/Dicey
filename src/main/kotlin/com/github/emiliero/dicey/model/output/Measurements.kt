c3d14bf8abb911b688599c966620862df5d1bea7
emiliero <emiliero@hiof.no>
2020-08-03 00:33:07 +0200

    Added measurements from 1-25

---
+++ src/main/kotlin/com/github/emiliero/dicey/model/output/Measurements.kt
@@ -0,0 +1,129 @@
+package com.github.emiliero.dicey.model.output
+
+enum class Measurements(var result: String) {
+    Measure1("1 cm (0.39 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure2("2 cm (0.78 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure3("3 cm (1.18 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure4("4 cm (1.57 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure5("5 cm (1.96 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure6("6 cm (2.36 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure7("7 cm (2.75 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure8("8 cm (3.14 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure9("9 cm (3.54 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure10("10 cm (3.93 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure11("11 cm (4.33 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure12("12 cm (4.72 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure13("13 cm (5.11 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure14("14 cm (5.51 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure15("15 cm (5.90 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure16("16 cm (6.29 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure17("17 cm (6.69 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure18("18 cm (7.08 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure19("19 cm (7.48 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure20("20 cm (7.87 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure21("21 cm (8.26 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure22("22 cm (8.66 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure23("23 cm (9.05 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure24("24 cm (9.44 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+    Measure25("25 cm (9.84 inches)"){
+        override fun toString(): String {
+            return "\n:flushed: That's a bit embarrassing."
+        }
+    },
+}
