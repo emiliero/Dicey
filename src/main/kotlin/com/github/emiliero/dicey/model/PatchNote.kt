eb1cfe065fe0ae170ca49557075f4ff79cc46c01
awheeler <awheeler@gamepoint.com>
2021-06-08 11:02:40 +0200

    removed annotations that are now not needed anymore and removed test patch note

--- src/main/kotlin/com/github/emiliero/dicey/model/PatchNote.kt
+++ src/main/kotlin/com/github/emiliero/dicey/model/PatchNote.kt
@@ -1,16 +1,9 @@
 package com.github.emiliero.dicey.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 class PatchNote(
-    @JsonProperty("version")
     val version: String,
-    @JsonProperty("date")
     val date: String,
-    @JsonProperty("new")
     val new: ArrayList<String>,
-    @JsonProperty("update")
     val update: ArrayList<String>,
-    @JsonProperty("removed")
     val removed: ArrayList<String>
 )
